b1361081c05b2a260ff06c7715af947d9254e9df
georgevetticaden <george.vetticaden@gmail.com>
2017-12-20 11:36:09 -0600

    updates

--- src/test/java/hortonworks/hdf/sam/refapp/trucking/env/TruckingRefAppEnviornmentBuilderTest.java
+++ src/test/java/hortonworks/hdf/sam/refapp/trucking/env/TruckingRefAppEnviornmentBuilderTest.java
@@ -10,8 +10,8 @@ public class TruckingRefAppEnviornmentBuilderTest {
 	
 	
 	protected static final String SAM_REST_URL = "http://hdf-3-1-build3.field.hortonworks.com:7777/api/v1";	
-	protected static final String SAM_EXTENSIONS_HOME = "/Users/gvetticaden/Dropbox/Hortonworks/HDP-Emerging-Products/A-HDF/A-Master-Demo/all-custom-extensions/3.1/3.1.0.0-270/Sam-Custom-Extensions";
-	protected static final String SAM_EXTENSIONS_VERSION = "0.0.11";
+	protected static final String SAM_EXTENSIONS_HOME = "/Users/gvetticaden/Dropbox/Hortonworks/HDP-Emerging-Products/A-HDF/A-Master-Demo/all-custom-extensions/3.1/3.1.0.0-404/Sam-Custom-Extensions";
+	protected static final String SAM_EXTENSIONS_VERSION = "3.1.0.0-404";
 	protected static final String SAM_CUSTOM_ARTIFACT_SUFFIX="_AUTOCREATED";
 	
 	TruckingRefAppEnviornmentBuilder envBuilder;
