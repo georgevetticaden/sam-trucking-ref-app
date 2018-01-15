d631681b5c4bdec72ed728985583a8af1997162d
georgevetticaden <george.vetticaden@gmail.com>
2018-01-15 17:54:10 -0600

    updating to build 501

--- src/main/java/hortonworks/hdf/sam/refapp/trucking/PropertiesConstants.java
+++ src/main/java/hortonworks/hdf/sam/refapp/trucking/PropertiesConstants.java
@@ -39,8 +39,8 @@ public class PropertiesConstants {
 
 	
 	/* Other Constants */
-	public static final String SAM_REF_APP_ADVANCE_FILE_LOCATION = "/3.1.0.0-420/streaming-ref-app-advanced.json";
-	public static final String SAM_REF_APP_FILE_LOCATION = "/3.1.0.0-420/streaming-ref-app.json";
+	public static final String SAM_REF_APP_ADVANCE_FILE_LOCATION = "/3.1.0.0-501/streaming-ref-app-advanced.json";
+	public static final String SAM_REF_APP_FILE_LOCATION = "/3.1.0.0-501/streaming-ref-app.json";
 
 	
 	
